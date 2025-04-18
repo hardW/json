60014d89e8262a7d8af4310dd95e3656e883f446
hardGd <hardnode@gmail.com>
2025-04-18 22:54:02 +0700

    spm build fix

--- Package.swift
+++ Package.swift
@@ -1,22 +1,48 @@
-// swift-tools-version: 5.9
+swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "nlohmann-json",
-     platforms: [
-        .iOS(.v12), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v4), .visionOS(.v1)
+    name: "json",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
-        .library(name: "json", targets: ["json"])
+        .library(
+            name: "json",
+            targets: ["json"]),
     ],
     targets: [
         .target(
             name: "json",
-            path: "single_include",
-            publicHeadersPath: "nlohmann"
-        )
+            dependencies: [],
+            path: ".",
+            exclude:
+                [
+                    "appveyor.yml",
+                    "benchmarks",
+                    "cmake",
+                    "ChangeLog.md",
+                    "doc",
+                    "include",
+                    "test",
+                    "third_party",
+                    "CMakeLists.txt",
+                    "CODE_OF_CONDUCT.md",
+                    "LICENSE.MIT",
+                    "Makefile",
+                    "meson.build",
+                    "nlohmann_json.natvis",
+                    "README.md",
+                    "wsjcpp.yml",
+                ],
+            sources:
+                [
+                    "_SwiftPackageManagerFile.cpp"
+                ],
+            publicHeadersPath: "./single_include/")
     ],
+
     cxxLanguageStandard: .cxx11
 )
